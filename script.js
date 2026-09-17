1b35414425f491e2c843ca830455f15f07d9d360
Gadz003 <dulzg003@gmail.com>
2026-09-17 20:57:15 -0300

    chore: função exibirProduto finalizado e perguntas respondidas

---
+++ script.js
@@ -0,0 +1,39 @@
+function buscarProduto(codigo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const produtos = {
+        101: { codigo: 101, nome: "Teclado", preco: 149.90 },
+        102: { codigo: 102, nome: "Mouse", preco: 79.90 },
+        103: { codigo: 103, nome: "Monitor", preco: 899.90 }
+      };
+
+      const produto = produtos[codigo];
+
+      if (produto) {
+        resolve(produto);
+      } else {
+        reject(new Error("Produto não encontrado."));
+      }
+    }, 1500);
+  });
+}
+
+
+async function exibirProduto(codigo) {
+    try {
+        const produto = await buscarProduto(codigo);
+        console.log("Buscando produto...");
+        console.log("Produto encontrado:", produto);
+    
+    } catch (error) {
+        console.log("Buscando produto...");
+        console.error("Erro ao buscar produto:", error.message);
+        
+    } finally {
+        console.log("Busca finalizada:");
+    }
+}
+
+
+exibirProduto(103);
+exibirProduto(109);
